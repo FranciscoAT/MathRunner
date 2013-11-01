2c5c2e1714231f632fa2dc49dd744edad14b3121
FranciscoAT <francisco.ad.trindade@gmail.com>
2013-11-01 12:32:06 -0400

    Week 0 Day 5 -
    Francisco
    mainMenu.html
    questionGenerator.js
    testValues.html

--- questionGenerator.js
+++ questionGenerator.js
@@ -1,5 +1,19 @@
+localStorage.operator = new Array(false, false, false, false);
 
 
-var test = function(a){
-	document.write(a);
+var setOperator = function (a) {
+	if (localStorage.operator[a] == false) {
+		localStorage.operator[a] = true;
+	} else {
+		localStorage.operator[a] = false;
+	}
+}
+
+var getOperator = function () {
+	for (var i = 0; i < localStorage.operator.length; i++) {
+		$('#P').append(localStorage.operator[i])
+	}
+}
+var test = function () {
+	document.write("test");
 }

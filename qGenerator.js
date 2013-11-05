eb98e6904acee55b0aaa4b7ca382d3644bd8740f
yeminlemon <yeminlemon@hotmail.com>
2013-11-05 12:35:47 -0500

    Week 1 Day 2

---
+++ qGenerator.js
@@ -0,0 +1,29 @@
+var operator = JSON.parse(localStorage["operator"]);
+var operation;
+var num;
+
+
+
+var getQuestion = function(){
+
+document.getElementById("demo").innerHTML=operator[0] + "y";
+//demo').append(operator[0] +"y")
+
+/*	do
+	{
+	
+		num = Math.floor(Math.random()*3);
+		}
+	while(!operator[num])
+	
+	operation = operators[num];
+		
+	var one = Math.floor(Math.random()*10 + 1), two = Math.floor(Math.random()*10 + 1);
+	
+	var question = one + " " + operation + " " + two + " = ";
+	
+	$('#question').append(question)
+	
+	*/
+
+}

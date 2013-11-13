8974397a69975577747f7fba49ceaa9f5bd6a23d
yeminlemon <yeminlemon@hotmail.com>
2013-11-13 10:58:55 -0500

    Week 2 Day 3
    Enhanced question generator
    -Created clickable multiple choice answers
    -one correct multiple choice answer
    -ensured subtraction results in positive value
    -ensured division results in whole number

--- qGenerator.js
+++ qGenerator.js
@@ -1,11 +1,13 @@
 var operator = JSON.parse(localStorage["operator"]);
 
-var question;
+var operation, one, two;
+var correct;
 
 getQuestion = function(){
 
 	document.getElementById("demo").innerHTML=operator;
 
+	//little check
 	var yay = false;
 	for(i=0;i<operator.length;i++)
 		if(operator[i])
@@ -13,44 +15,73 @@ getQuestion = function(){
 	
 	if(!yay)
 		operator[0]=true;
+	//end little check
 
 	var num;
 	do{
-		num = Math.floor(Math.random()*3);
+		num = Math.floor(Math.random()*4);
 	}
 	while(operator[num] == false);
 	
-	var operation;
+	one = Math.floor(Math.random()*10 + 1), two = Math.floor(Math.random()*10 + 1);
+	
 	switch(num){
 	case(0):
-	operation = "+";
-	break;
+		operation = "+";
+		break;
 	case(1):
-	operation = "-";
-	break;
+		operation = "-";
+		if(one < two){
+			var temp = one;
+			one = two;
+			two = temp;
+		} //to avoid negative answer values, make sure larger number goes first
+		break;
 	case(2):
-	operation = "*";
-	break;
+		operation = "*";
+		break;
 	case(3):
-	operation = "/";
-	break;
+		operation = "/";
+		switcher = true;
+		one *= two; //to avoid decimal answer values, set one to the product of one and two so that the quotient is a whole number
+		break;
 	}
 	
-	var one = Math.floor(Math.random()*10 + 1), two = Math.floor(Math.random()*10 + 1);
-	
-	question = one + " " + operation + " " + two + " = ";
+	var question = one + " " + operation + " " + two + " = ";
 	
 	document.getElementById("question").innerHTML=question;
 }
 
 getAnswer = function(){
-	if(operation == "+")
+
+	var answer;
+	
+	switch(operation){
+	case("+"):
+		answer = one+two;
+		break;
+	case("-"):
+		answer = one-two;
+		break;
+	case("*"):
+		answer = one*two;
+		break;
+	case("/"):
+		answer = one/two;
+		break;
+	}
+	
+	correct = Math.floor(Math.random()*4+1);
 		
+	$("#answer" + correct).attr("value", answer)
+
+}
+
+isCorrect = function(button){
 	
-	if(operation == "-")
-	
-	if(operation == "*")
-	
-	if(operation == "/")
+	if(button == correct)
+		document.getElementById("test").innerHTML="CORRECT!";
+	else
+		document.getElementById("test").innerHTML="WRONG";
 
 }

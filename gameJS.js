c420cf6eaaf171a27bdb12fc8c4022bc6299af37
FranciscoAT <francisco.ad.trindade@gmail.com>
2013-12-10 11:10:11 -0500

    Week 9 Day 2

--- gameJS.js
+++ gameJS.js
@@ -1,8 +1,37 @@
-var c=document.getElementById("background");
-var ctx=c.getContext("2d");
-ctx.fillStyle="#FF0000";
-ctx.fillRect(75,113,30,20);
-ctx.fillRect(150,113,30,20);
-ctx.fillRect(225,113,30,20);
+function animate(myRectangle, canvas, ctx) {
+	var x = 0;
+		function test(){
+			setTimeout(function(){	
+				myRectangle.x += x;
+				$('#temp').append(myRectangle.x)
+				drawRectangle(myRectangle, ctx);
+				ctx.clearRect(x,75,50,25);
+				x += 2;				
+				test();
+			},10);
+		}
+}	
 
+function drawRectangle(myRectangle, ctx) {
+	ctx.beginPath();
+	ctx.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
+	ctx.fillStyle = '#8ED6FF';
+	ctx.fill();
+	ctx.lineWidth = myRectangle.borderWidth;
+	ctx.strokeStyle = 'black';
+	ctx.stroke();
+}
 
+var myRectangle = {
+x: 0,
+y: 75,
+width: 50,
+height: 25,
+borderWidth: 2.5
+};
+	  
+	  
+var canvas = document.getElementById('background');
+var ctx = canvas.getContext('2d');
+
+animate(myRectangle, canvas, ctx);

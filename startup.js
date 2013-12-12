a9861f7da98d8c6db79df561af7ab00fc2df31cd
FranciscoAT <francisco.ad.trindade@gmail.com>
2013-12-12 13:04:23 -0500

    Week 9 Day 4 #2

--- startup.js
+++ startup.js
@@ -1,13 +1,33 @@
+var selected = "null";
+var name = "null";
 
 setGender = function(g){
-
+if (selected == "boy"){
+	if(g == "boy"){
+		g = "null";
+	}
+	else{
+		$("#boy").toggleClass('colorToggle')
+	}
+}
+else if(selected == "girl"){
+	if(g == "girl"){
+		g = "null";
+	}
+	else{
+		$("#girl").toggleClass('colorToggle')
+	}
+}	
 localStorage.gender = g;
+selected = g;
 
-document.getElementById("demo").innerHTML=localStorage.gender + "h";
+document.getElementById("demo").innerHTML=localStorage.gender;
 }
 
 setName = function(){
+name = document.getElementById("name").value;
 localStorage.name = document.getElementById("name").value;
+goToMain();
 }
 
 getPlayer = function(){
@@ -15,4 +35,13 @@ $('#demo').append(localStorage.gender)
 //document.getElementById("demo").innerHTML=localStorage.gender + "y";
 
 $('#demo').append(localStorage.name)
+}
+
+goToMain = function () {
+	if(selected !== "null" && name !=="null" || name !==""){
+		location.href= "mainMenu.html";
+	}
+	else{
+		alert("Name or Gender is not Selected!");
+	}
 }

6c972ed66a5c4d7e8327f78888012e18f1872a54
FranciscoAT <francisco.ad.trindade@gmail.com>
2013-12-18 11:09:53 -0500

    Week 10 Day 3

--- gameJS.js
+++ gameJS.js
@@ -1,18 +1,16 @@
-function animate(myRectangle, canvas, ctx, x) { 
+function animate(myRunner, canvas, ctx, x) { 
 	var running = true;
-	
-	
+	drawRunner(myRunner, ctx);
 	part1();
 	function part1(){
 		if(running == true){
-			myRectangle.x = x;
-			ctx.clearRect(x-5,70,60,35);
-			drawRectangle(myRectangle, ctx);
+			myRunner.x = x;
+			ctx.clearRect(x-5,myRunner.y-5,myRunner.width+10,myRunner.height+10);
 			x += 1;
-			
+			drawRunner(myRunner, ctx);
 			var test = setTimeout(part1, 10);
 		
-			if(x == $("#background").width()-myRectangle.width){
+			if(x == $("#background").width()-myRunner.width){
 				running = false;
 				clearTimeout(test);
 			}
@@ -20,19 +18,16 @@ function animate(myRectangle, canvas, ctx, x) {
 	}
 }	
 
-function drawRectangle(myRectangle, ctx) {
-	ctx.beginPath();
-	ctx.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
-	ctx.fillStyle = '#8ED6FF';
-	ctx.fill();
-
+function drawRunner(myRunner, ctx) {
+	ctx.drawImage(myRunner.image, myRunner.x, myRunner.y, myRunner.width, myRunner.height);
 }
 
-var myRectangle = {
+var myRunner = {
+image:"male1.png",
 x: 0,
 y: 75,
-width: 50,
-height: 25,
+width: 80,
+height: 140,
 };
 	  
 var x = 0;
@@ -41,4 +36,6 @@ canvas.height=600;
 canvas.width=724;
 var ctx = canvas.getContext('2d');
 
-animate(myRectangle, canvas, ctx, x);
+//drawRunner(myRunner, ctx);
+ctx.drawImage("male1.png", 10,10,80,140);
+//animate(myRunner, canvas, ctx, x);

755c1b239db86aae1e20269b0bce2f3d12553b17
yeminlemon <yeminlemon@hotmail.com>
2014-01-04 14:02:23 -0500

    Break Tyme

--- qGenerator.js
+++ qGenerator.js
@@ -1,22 +1,25 @@
-var operator = JSON.parse(localStorage["operator"]);
+var operator = JSON.parse(localStorage["operator"]); //to obtain selected opertors
 
-var operation, one, two;
+var operation, one, two; //to generate question
 
-var correct;
+var correct; //is either 1-4, the correct multiple choice answer
 
-var points = 0;
-var coins = 0;
+var points = 0; //number of points
+var coins = 0; //number of coins
 
-var energy = 100;
+var energy = 100; //amount of energy
 
-var questions = 0;
+var questions = 0; //number of questions answered
 
-var difficulty = 0;
+var difficulty = 0; //question difficulty
 
-var countdown;
+var countdown; //timer variable
 
 getQuestion = function(){
 
+	clearInterval(countdown); //used to reset timer when new question is generated
+
+	//****check
 	document.getElementById("demo").innerHTML=operator;
 
 	//little check
@@ -29,13 +32,14 @@ getQuestion = function(){
 		operator[0]=true;
 	//end little check
 	
-	//generating question with random operator 
+	//generating question with random operator chosen out of selected operators
 	var num;
 	do{
 		num = Math.floor(Math.random()*4);
 	}
 	while(operator[num] == false);
 	
+	//********FIX THIS: difficulty
 	switch(difficulty){
 	case(0):
 		one = Math.floor(Math.random()*10 + 1), two = Math.floor(Math.random()*10 + 1);
@@ -47,8 +51,6 @@ getQuestion = function(){
 		one = Math.floor(Math.random()*100+1), two = Math.floor(Math.random()*100+1);
 		break;
 	}
-	
-	prompt(operation + " " + one + " " + two);
 	
 	switch(num){
 	case(0):
@@ -72,10 +74,10 @@ getQuestion = function(){
 	}
 	
 	var question = one + " " + operation + " " + two + " = ";
-	
+	//****check
 	document.getElementById("question").innerHTML=question;
 	
-	//generating answer with one correct and three incorrect answers
+	//generating answers with one correct and three incorrect answers
 	var answer;
 	
 	switch(operation){
@@ -93,42 +95,44 @@ getQuestion = function(){
 		break;
 	}
 	
-	correct = Math.floor(Math.random()*4+1);
-		
-	$("#answer" + correct).attr("value", answer)
+	correct = Math.floor(Math.random()*4+1); 
+	$("#answer" + correct).attr("value", answer) //picking one multiple choice to be correct and setting it there
 	
-	prompt(correct);
+	var otherAnswer; //the other answer being generated
+	var old = []; //all the old answers already generated to make sure the multiple choice answers are all different
 	
-	for(var i = 1; i < 5; i++)
+	for(var i = 1; i < 5; i++) //for filling in other multiple choice answers
 	{
-		
 		if(correct != i)
 		{
-			prompt("loop" + i);
+			old.push(otherAnswer); //add old answers onto array
 			
-			var otherAnswer;
-			
-			//make sure other multiple choice answers are not the same as the correct answer
-			//*****FIX THIS: make sure there are no two of the same wrong answers; make sure range of answers is sufficient for lower number answers
 			do{
-			if(answer == 0)
-				answer = 1;
-			otherAnswer = Math.floor(Math.random()*(answer*3)+answer/2);
-			prompt(otherAnswer);
-			}while(otherAnswer == answer)
+				otherAnswer = Math.floor(Math.random()*(answer*2 + 5)+(answer/2 + 1)); //algorithm for a good range of answers
+			
+				var discontinue = false; //variable used to make sure multiple choice answers are all different before continuing
+			
+				for(var j = 0; j < old.length; j++)
+					if(old[j] == otherAnswer) discontinue = true; //make sure that all the multiple choice answers are different from each other
+			
+			}while(otherAnswer == answer || discontinue) //make sure other multiple choice answers are not the same as the correct answer or other answers
 			
 			$("#answer" + i).attr("value", otherAnswer)
 		}
 	}
 	
-	prompt("hi");
-	startTimer(10);
+	startTimer(10); //timer starts after question and answer are generated
 }
 
 //****FIX THIS: timer spam; var countdown
 startTimer = function(timer){
-	countdown = setInterval(function(){document.getElementById("timer").innerHTML=timer;
-	if(timer > 0) timer -= 1; else{clearInterval(countdown);wrongAns("timeout")}}, 1000);
+	document.getElementById("timer").innerHTML=timer; //timer appears right away without the initial delay
+	timer --; //timer decreases by one because of initial delay to avoid an extra second
+	countdown = setInterval(function(){
+		document.getElementById("timer").innerHTML=timer;
+		if(timer > 0) timer -= 1; 
+		else{clearInterval(countdown);wrongAns("timeout")}
+		}, 1000); //after a delay, the timer will appear and timer will begin countdown until 0
 }
 
 isCorrect = function(button){
@@ -155,12 +159,15 @@ rightAns = function(){
 	coins += 10;
 	localStorage.points = points;
 	localStorage.coins = coins;
+	//****check
 	document.getElementById("test").innerHTML="CORRECT!" + localStorage.points + "coins:" + coins;
 }
 
+//****ADD THE REASONS FOR THE TIMEOUT
 wrongAns = function(reason){
 	energy -= 5;
 	localStorage.energy = energy;
+	//****check
 	document.getElementById("test").innerHTML="WRONG" + localStorage.energy;
 	
 	if(reason == "timeout")
